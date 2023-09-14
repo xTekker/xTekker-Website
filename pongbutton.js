23a468eed2c7086805b79817d596e2863a6321da
Nathan <68022353+xTekker@users.noreply.github.com>
2023-09-14 13:44:10 +0100

    Delete pongbutton.js

--- pongbutton.js
+++
@@ -1,8 +0,0 @@
-// Get the reference to the button element
-const pongButton = document.getElementById("pong-button");
-
-// Add a click event listener to the button
-pongButton.addEventListener("click", () => {
-  // Navigate to pong.html
-  window.location.href = "pong.html";
-});
