769a17776281846ea81b81f5c8629a074c4d77aa
Nathan <68022353+xTekker@users.noreply.github.com>
2023-09-14 13:43:27 +0100

    Delete snakebutton.js

--- snakebutton.js
+++
@@ -1,8 +0,0 @@
-// Get the reference to the button element
-const snakeButton = document.getElementById("snake-button");
-
-// Add a click event listener to the button
-snakeButton.addEventListener("click", () => {
-  // Navigate to snake.html
-  window.location.href = "snake.html";
-});
